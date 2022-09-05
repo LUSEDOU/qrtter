a476d05a6bacae23cc43a11514ef5cb740cae039
LUSEDOU <dolorier_uribe@hotmail.com>
2022-09-05 08:01:45 -0500

    chore(reader): initialize

---
+++ lib/reader/view/reader_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ReaderPage extends StatelessWidget {
+  const ReaderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ReaderView();
+  }
+}
+
+class ReaderView extends StatelessWidget {
+  const ReaderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
