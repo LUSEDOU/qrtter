c482872910354268df3683b1a05d10c5b12ab9aa
LUSEDOU <dolorier_uribe@hotmail.com>
2022-09-05 20:10:38 -0500

    feat: create generator body

--- lib/generator/view/generator_page.dart
+++ lib/generator/view/generator_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qrtter/generator/generator.dart';
 
 class GeneratorPage extends StatelessWidget {
   const GeneratorPage({super.key});
 
+  Route<dynamic> get route => MaterialPageRoute<void>(
+        builder: (context) => this,
+      );
+
   @override
   Widget build(BuildContext context) {
-    return const GeneratorView();
+    return BlocProvider(
+      create: (context) => GeneratorBloc(),
+      child: const GeneratorView(),
+    );
   }
 }
 
@@ -16,8 +26,53 @@ class GeneratorView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: Text('Generator'),
+      body: BlocListener<GeneratorBloc, GeneratorState>(
+        listener: (context, state) {
+          if (state.status == GeneratorStatus.failure) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Something went wrong'),
+              ),
+            );
+          }
+        },
+        child: SingleChildScrollView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              const Text('Generate QR Code'),
+              const SizedBox(height: 16),
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Enter text to generate QR Code',
+                ),
+                onChanged: (value) => context
+                    .read<GeneratorBloc>()
+                    .add(GeneratorQrChanged(qrText: value)),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () => context
+                    .read<GeneratorBloc>()
+                    .add(const GeneratorQrSubmit()),
+                child: const Text('Generate'),
+              ),
+              const SizedBox(height: 16),
+              BlocBuilder<GeneratorBloc, GeneratorState>(
+                buildWhen: (previous, current) =>
+                    previous.qrCode != current.qrCode &&
+                    current.status == GeneratorStatus.success,
+                builder: (context, state) {
+                  return QrImage(
+                    data: state.qrCode ?? '',
+                  );
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
